4e3e66d85d3ffad153ab151c63c53a1bdbb09399
Woonghui Won <whw0111@gmail.com>
2019-03-31 13:55:15 -0700

    Simple Vending Machine code created from HTML/CSS/Javascript

--- VendingMachine.js
+++ VendingMachine.js
@@ -5,21 +5,61 @@ function completeMessage() {
     } else {
         txt = "Option Cancelled";
     }
-    document.getElementById("message").innerHTML = txt;
+    document.getElementById("order").innerHTML = txt;
 }
 
 function cancelMessage() {
-    var txt;
+    var text;
     if (confirm("Would you like to cancel these orders?")) {
-        txt = "Order Cancelled";
+        text = "Order Cancelled";
     } else {
-        txt = "Option Cancelled";
+        text = "Option Cancelled";
     }
-    document.getElementById("message").innerHTML = txt;
+    document.getElementById("order").innerHTML = text;
 }
 
-var clickOrder = 0;
-function myCost() {
-    clickOrder += 1.50;
-    document.getElementById("order").innerHTML = "Order Added: $" + clickOrder;
+function myOrder() {
+    var order = document.getElementById("myInput").value;
+
+    switch(order) {
+        case "A1":
+            completeMessage();
+            break;
+        case "A2":
+            completeMessage();
+            break;
+        case "A3":
+            completeMessage();
+            break;
+        case "B1":
+            completeMessage();
+            break;
+        case "B2":
+            completeMessage();
+            break;
+        case "B3":
+            completeMessage();
+            break;
+        case "C1":
+            completeMessage();
+            break;
+        case "C2":
+            completeMessage();
+            break;
+        case "C3":
+            completeMessage();
+            break;
+        case "D1":
+            completeMessage();
+            break;
+        case "D2":
+            completeMessage();
+            break;
+        case "D3":
+            completeMessage();
+            break;
+        default:
+            text = "Invalid Input. Please Try Again.";
+    }
+    document.getElementById("order").innerHTML = text;
 }
